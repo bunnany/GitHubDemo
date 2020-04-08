b422fe57e29148053409e85d74885eaf40ccf254
Bunna Ny <bunna.ny@onslow.school.nz>
2020-04-08 17:43:52 +1200

    Created new SomeCode class

---
+++ SomeCode.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class SomeCode here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SomeCode
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class SomeCode
+     */
+    public SomeCode()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        System.out.println("Hello, World!");
+        return x + y;
+    }
+}
